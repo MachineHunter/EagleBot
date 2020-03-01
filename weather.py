2954cb8fd15cf736e6e522e9c63b9925c3f1f2ea
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-03-01 17:11:29 +0900

    added weather.py

--- weather.py
+++ weather.py
@@ -3,20 +3,31 @@ import requests as req
 import datetime
 
 
-url = "https://weather.yahoo.co.jp/weather/jp/13/4410/13209.html"
-r = req.get(url)
 
-soup = BeautifulSoup(r.text, "html.parser")
+def getWeather():
+	url = "https://weather.yahoo.co.jp/weather/jp/13/4410/13209.html"
+	r = req.get(url)
 
-weather_table = soup.select("table.yjw_table2")[0]
-current_time = str(datetime.datetime.now()).split(" ")[1]
-current_hour = current_time.split(":")[0]
-print(current_hour)
+	soup = BeautifulSoup(r.text, "html.parser")
 
-for tr in weather_table.find_all("tr"):
-	for td in tr.find_all("td"):
-		td = td.text.replace("\n", "")
-		td = td.replace(' ', '')
-		td = td.ljust(15)
-		print("|"+td+"|", end="")
-	print()
+	weather_table = soup.select("table.yjw_table2")[0]
+
+
+	current_time = str(datetime.datetime.now()).split(" ")[1]
+	current_time = current_time.split(":")[0]+":"+current_time.split(":")[1]
+	result = current_time+"\n"
+
+
+	col = len(weather_table.find_all("tr")[0].find_all("td"))
+	weathercols = [[]*1 for i in range(9)]
+
+	for tr in weather_table.find_all("tr")[0:3]:
+		for i,td in enumerate(tr.find_all("td")):
+			td = td.text.replace("\n", "")
+			weathercols[i].append(td)
+
+	for i,weather in enumerate(weathercols):
+		if(i==0): continue
+		result += str(weather)+"\n"
+
+	return result
